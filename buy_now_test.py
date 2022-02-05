5b84a4a9be19389665efe360d5875f56b51af8bd
shaojiewang <wsjmessi@163.com>
2022-02-05 20:40:25 +0800

    fix hangging in settlement

--- buy_now_test.py
+++ buy_now_test.py
@@ -4,7 +4,8 @@ print(now)
 import time
 #全自动化Python代码操作
 from selenium import webdriver
-times = "2022-01-24 19:59:59.50000000"
+from selenium.webdriver.common.by import By
+times = "2022-02-03 20:02:59.50000000"
 browser = webdriver.Chrome()
 
 browser.maximize_window()
@@ -15,7 +16,7 @@ if browser.find_element_by_link_text("亲，请登录"):
     browser.find_element_by_link_text("亲，请登录").click()
 
 print(f"请尽快扫码登录")
-time.sleep(30)
+time.sleep(15)
 browser.get("https://cart.taobao.com/cart.htm")
 time.sleep(3)
 
@@ -27,6 +28,7 @@ while True:
     except:
         print(f"找不到购买按钮")
 
+flag = 1
 while True:
     #获取电脑现在的时间,                      year month day
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
@@ -36,21 +38,34 @@ while True:
     if now > times:
         # 点击结算按钮
         while True:
+            if flag == 0:
+                break
             try:
                 if browser.find_element_by_link_text("结 算"):
                     print("here")
-                    browser.find_element_by_link_text("结 算").click()
+                    a = browser.find_element_by_link_text("结 算")
+                    print(a)
+                    c = a.click()#webdriver.ActionChains(browser).move_to_element(a).click(a).perform()
+                    for i in range(5):
+                        c = a.click()
+                        time.sleep(0.01)
+                    print(c)
+                    flag = 0
                     print(f"主人,程序锁定商品,结算成功")
                     break
             except:
                 pass
-        # 点击提交订单按钮
-        while True:
-            try:
-                if browser.find_element_by_link_text('提交订单'):
-                    browser.find_element_by_link_text('提交订单').click()
-                    print(f"抢购成功，请尽快付款")
-            except:
-                print(f"主人,我已帮你抢到商品啦,您来支付吧")
-                break
+            time.sleep(0.01)
+    # 点击提交订单按钮
+    while True:
+        try:
+            if browser.find_element(By.XPATH, '//a[text()="提交订单"]'):
+                browser.find_element(By.XPATH, '//a[text()="提交订单"]').click()
+                print(f"抢购成功，请尽快付款")
+        except:
+            print(f"主人,我已帮你抢到商品啦,您来支付吧")
+            #browser.quit()
+            break
         time.sleep(0.01)
+    
+    #break
