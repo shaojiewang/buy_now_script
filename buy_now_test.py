b85dedbac256df5f13ad03e3db6f1921aaf5c577
shaojiewang <wsjmessi@163.com>
2022-02-08 20:01:18 +0800

    automatically changing date

--- buy_now_test.py
+++ buy_now_test.py
@@ -1,11 +1,14 @@
 import datetime #模块
-now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+now_t = datetime.datetime.now()
+now = now_t.strftime('%Y-%m-%d %H:%M:%S.%f')
 print(now)
 import time
 #全自动化Python代码操作
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-times = "2022-02-03 20:02:59.50000000"
+wait_t = now_t.replace(now_t.year, now_t.month, now_t.day, 19, 59, 59, 500000)
+str_wait_t=wait_t.strftime('%Y-%m-%d %H:%M:%S.%f')
+#str_wait_t = "2022-02-03 19:59:59.50000000"
 browser = webdriver.Chrome()
 
 browser.maximize_window()
@@ -35,7 +38,7 @@ while True:
     # 对比时间，时间到的话就点击结算
     print(now)
     #判断是不是到了秒杀时间?
-    if now > times:
+    if now > str_wait_t:
         # 点击结算按钮
         while True:
             if flag == 0:
@@ -56,16 +59,14 @@ while True:
             except:
                 pass
             time.sleep(0.01)
-    # 点击提交订单按钮
-    while True:
-        try:
-            if browser.find_element(By.XPATH, '//a[text()="提交订单"]'):
-                browser.find_element(By.XPATH, '//a[text()="提交订单"]').click()
-                print(f"抢购成功，请尽快付款")
-        except:
-            print(f"主人,我已帮你抢到商品啦,您来支付吧")
-            #browser.quit()
-            break
-        time.sleep(0.01)
-    
-    #break
+        # 点击提交订单按钮
+        while True:
+            try:
+                if browser.find_element(By.XPATH, '//a[text()="提交订单"]'):
+                    browser.find_element(By.XPATH, '//a[text()="提交订单"]').click()
+                    print(f"抢购成功，请尽快付款")
+            except:
+                print(f"主人,我已帮你抢到商品啦,您来支付吧")
+                #browser.quit()
+                break
+            time.sleep(0.01)
